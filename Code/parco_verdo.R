5a4811f5d4e43a2a54fd78bcdba203bff15ecf8d
Matthias Bieri <bierimatthias92@gmail.com>
2023-02-16 20:23:23 +0100

    More messy upload

---
+++ Code/parco_verdo.R
@@ -0,0 +1,33 @@
+# https://www.neubauprojekte.ch/listing/kaufen-mieten/schwyz/schuebelbach/parco-verde-schuebelbach/
+
+library(rvest)
+library(dplyr)
+library(stringr)
+# Read the URL of the website into R
+url <- "https://www.neubauprojekte.ch/listing/kaufen-mieten/schwyz/schuebelbach/parco-verde-schuebelbach"
+webpage <- read_html(url)
+
+# Adress:
+adress <- "Bahnweg, 8862, Schübelbach"
+
+# Use html_nodes to extract the elements with class "list scroll"
+houses <- html_nodes(webpage, ".px-3") %>% html_text()
+houses
+
+# assume that 'houses' is the name of your list of house listings
+
+# extract number of rooms from each string
+rooms <- gsub(".*?(\\d+\\.\\d+)\\s*Zimmer.*", "\\1", houses)
+
+# extract size of flat from each string
+sizes <- gsub(".*?(\\d+)\\s*m².*", "\\1", houses)
+
+# extract price from each string
+prices <- gsub(".*?(CHF\\s*[\\d\\'\\.]+).*", "\\1", houses)
+# remove any apostrophes from the price strings
+prices <- gsub("'", "", prices)
+prices <- stringr::str_extract(prices, "(?<=CHF\\s)[\\d\\.]+")
+
+# combine the results into a data frame
+house_data <- data.frame(rooms, sizes, prices)
+house_data$adress <- rep(adress)
